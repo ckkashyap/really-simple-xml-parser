04c37bbcb34d0f7dc29a909ba0090fad6d2a80de
Kashyap <kashyap@kashyap-ubuntu.(none)>
2012-07-24 18:49:10 +0530

    working version

--- RSXP.hs
+++ RSXP.hs
@@ -1,7 +1,105 @@
-module RSXP where
+module RSXP (
+    XMLAST (Element, Body, Comment)
+  , parseXML
+) where
 
-f :: Int -> Int
-f x = x + 10
+import Text.ParserCombinators.Parsec
 
-g :: Int -> Int -> Int
-g x y= x + y
+data XMLAST =  
+    Element Name [Attribute] [XMLAST]
+  | Body String
+  | Comment String
+  | CouldNotParse
+  deriving Show
+
+type Name      = String
+type Attribute = (Key, Value)
+type Key       = String
+type Value     = String 
+
+
+parseXML :: String -> XMLAST
+parseXML str =
+  f ast where
+      ast = parse (spaces >> xmlParser) "" str
+      f (Right x) = x
+      f (Left x) = CouldNotParse
+      
+xmlParser :: Parser XMLAST
+xmlParser = 
+  try withoutExplictCloseTag <|>  withExplicitCloseTag
+
+
+withExplicitCloseTag :: Parser XMLAST
+withExplicitCloseTag = 
+  do
+    (name, attr) <- openTag
+    innerXML <- many innerXML
+    closeTag name
+    return (Element name attr innerXML)
+
+innerXML = comment <|> xmlParser <|> getBody
+
+getBody = fmap Body $ many1 $ noneOf "<>"
+
+comment :: Parser XMLAST
+comment =
+  do
+    try $ string "<!--"
+    body <- manyTill anyChar (string "-->")
+    return (Comment body)
+
+openTag :: Parser (String, [(String,String)])
+openTag =
+  do
+    try $ char '<' >> notFollowedBy (char '/')
+    tag <- many (letter <|> digit)
+    spaces
+    a <- try (many keyValue)
+    char '>'
+    return (tag, a)
+
+closeTag :: String -> Parser ()
+closeTag str =
+  do
+    try $ string "</"
+    spaces
+    string str
+    spaces
+    char '>'
+    return ()
+
+withoutExplictCloseTag :: Parser XMLAST
+withoutExplictCloseTag = 
+  do
+    try $ char '<' >> notFollowedBy (char '/')
+    name <- many (letter <|> digit)
+    spaces
+    a <- try (many keyValue)
+    spaces
+    string "/>"
+    return (Element name a [])
+
+keyValue :: Parser (String, String)
+keyValue = 
+  do
+    key <- many1 (letter <|> digit)
+    spaces
+    char '='
+    spaces
+    value <- quotedString
+    spaces
+    return (key, value)
+
+quotedString :: Parser String
+quotedString = 
+  do 
+    c <- (try (char '"')) <|> char '\''
+    content <- many (quotedChar c)
+    char c <?> "quote at end"
+    return content
+
+quotedChar :: Char -> Parser Char
+quotedChar c =
+  try (string ['\\', c] >> return c)        
+  <|> noneOf [c]
